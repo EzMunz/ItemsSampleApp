9c625cd99c8582d65f36fc9c1bdf97d0be8f29ba
Ezequiel Munz <emunz@sparkdigital.com>
2019-11-01 19:12:29 -0300

    Initial Commit

---
+++ MobileInterview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileInterview
+//
+//  Created by Ezequiel Facundo Munz on 01/11/2019.
+//  Copyright © 2019 Ezequiel Munz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
